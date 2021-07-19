442e5ea76aefe59d06737488ddc97df5f5eff328
Atsushi Hashimoto <atsushi.hashimoto@sinicx.com>
2021-07-19 18:53:34 +0900

    add javascript for animation

--- my_app.py
+++ my_app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__, static_folder=".", static_url_path='')
 
@@ -7,14 +7,18 @@ def index():
     return app.send_static_file('index.html')
 
 @app.route('/task/<hypara_set_id>')
-def task():
+def task(hypara_set_id):
 
-    reference1 = "templates/blue.png"
-    reference2 = "templates/red.png"
-    target = "templates/cat.png"
+    reference1 = "/templates/blue.png"
+    reference2 = "/templates/red.png"
+    target = "/templates/cat.png"
     annotator_id = "Anno_test"
 
-    hyperparameters = {} # load from sql table.
+    hyperparameters = {
+        'reference_dulation': 2000, #msec
+        'target_dulation': 2000,
+        'slider_dulation': 4000,
+    } # load from sql table.
 
     return render_template(
         'task.html',
@@ -24,4 +28,16 @@ def task():
         hypara_set_id=hypara_set_id,
         hyperparameters=hyperparameters,
         )
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    reference1 = request.form['reference1']
+    reference2 = request.form['reference2']
+    target=request.form['target']
+    annotator_id = request.form['annotator_id']
+    hypara_set_id = request.form['hypara_set_id']
+    timestamp = request.form['timestamp']
+
+    print(reference1, reference2, target, annotator_id, hypara_set_id, timestamp)
+
 app.run(port=8000,debug=True)
