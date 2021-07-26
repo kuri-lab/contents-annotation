4f5efe57c3565b9ccfddc8c129b8a5d99ca26f14
golden77777 <komei15821185@gmail.com>
2021-07-26 17:59:41 +0900

    add databases

--- app/my_app.py
+++ app/my_app.py
@@ -1,5 +1,9 @@
 from flask import Flask, render_template, request
 import random
+from models.models import OnegaiContent
+#以下を追加
+from models.database import db_session
+from datetime import datetime
 
 app = Flask(__name__, static_folder=".", static_url_path='')
 
@@ -37,6 +41,16 @@ def task(hypara_set_id):
         hypara_set_id=hypara_set_id,
         hyperparameters=hyperparameters,
         )
+
+#以下を追加
+@app.route("/add",methods=["post"])
+def add():
+    ref1 = request.form["ref1"]
+    ref2 = request.form["ref2"]
+    content = OnegaiContent(ref1,ref2,datetime.now())
+    db_session.add(content)
+    db_session.commit()
+    return index()
 
 @app.route('/submit', methods=['POST'])
 def submit():
---
+++ models/models.py
@@ -0,0 +1,19 @@
+from sqlalchemy import Column, Integer, String, Text, DateTime, Float
+from models.database import Base
+from datetime import datetime
+
+
+class OnegaiContent(Base):
+    __tablename__ = 'onegaicontents'
+    id = Column(Integer, primary_key=True)
+    ref1 = Column(Float)
+    ref2 = Column(Float)
+    date = Column(DateTime, default=datetime.now())
+
+    def __init__(self, ref1=None, ref2=None, date=None):
+        self.ref1 = ref1
+        self.ref2 = ref2
+        self.date = date
+
+    def __repr__(self):
+        return '<Title %r>' % (self.title)
