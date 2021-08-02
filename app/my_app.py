d32d96afbde083dc9cc21d34e1704566ae25ac07
golden77777 <komei15821185@gmail.com>
2021-08-02 11:43:46 +0900

    add login

--- app/my_app.py
+++ app/my_app.py
@@ -1,15 +1,27 @@
 from flask import Flask, render_template, request
 import random
-from models.models import ImpressionContent
+from models.models import ImpressionContent,User
 #以下を追加
 from models.database import db_session
 from datetime import datetime
+from flask import session,redirect,url_for
+from app import key
+from hashlib import sha256
 
 app = Flask(__name__, static_folder=".", static_url_path='')
+app.secret_key = key.SECRET_KEY
 
 @app.route('/')
+# @app.route("/index")
 def index():
-    return app.send_static_file('index.html')
+    if "user_name" in session:
+        name = session["user_name"]
+        impression = ImpressionContent.query.all()
+        return redirect(url_for("/task/<hypara_set_id>"))
+    else:
+        return redirect(url_for("top"))
+# def index():
+#     return app.send_static_file('index.html')
 
 @app.route('/task/<hypara_set_id>')
 def task(hypara_set_id):
@@ -45,6 +57,7 @@ def task(hypara_set_id):
 #以下を追加
 @app.route("/add",methods=["post"])
 def add():
+    name = session.get('user_name', None)
     reference1 = request.form['reference1']
     reference2 = request.form['reference2']
     target = request.form['target']
@@ -53,7 +66,7 @@ def add():
     ref2 = - ref1
     # annotator_id = request.form['annotator_id']
     # hypara_set_id = request.form['hypara_set_id']
-    content = ImpressionContent(reference1,reference2,target, ref1,ref2,datetime.now())
+    content = ImpressionContent(name, reference1,reference2,target, ref1,ref2,datetime.now())
     db_session.add(content)
     db_session.commit()
     return index()
@@ -69,5 +82,57 @@ def submit():
     timestamp = request.form['timestamp']
 
     print(reference1, reference2, target, annotator_id, hypara_set_id, timestamp)
+
+
+@app.route("/top")
+def top():
+    status = request.args.get("status")
+    return render_template("top.html",status=status)
+
+
+@app.route("/login",methods=["post"])
+def login():
+    user_name = request.form["user_name"]
+    user = User.query.filter_by(user_name=user_name).first()
+
+    if user:
+        password = request.form["password"]
+        hashed_password = sha256((user_name + password + key.SALT).encode("utf-8")).hexdigest()
+        if user.hashed_password == hashed_password:
+            session["user_name"] = user_name
+            return (redirect(url_for("add")),redirect(url_for("index")))
+        else:
+            return redirect(url_for("top",status="wrong_password"))
+    else:
+        return redirect(url_for("top",status="user_notfound"))
+
+
+@app.route("/newcomer")
+def newcomer():
+    status = request.args.get("status")
+    return render_template("newcomer.html",status=status)
+
+
+@app.route("/registar",methods=["post"])
+def registar():
+    user_name = request.form["user_name"]
+    user = User.query.filter_by(user_name=user_name).first()
+    if user:
+        return redirect(url_for("newcomer",status="exist_user"))
+    else:
+        password = request.form["password"]
+        hashed_password = sha256((user_name + password + key.SALT).encode("utf-8")).hexdigest()
+        user = User(user_name, hashed_password)
+        db_session.add(user)
+        db_session.commit()
+        session["user_name"] = user_name
+        return redirect(url_for("task"))
+
+
+@app.route("/logout")
+def logout():
+    session.pop("user_name", None)
+    return redirect(url_for("top",status="logout"))
+
 
 app.run(port=8000,debug=True)
--- models/models.py
+++ models/models.py
@@ -6,6 +6,7 @@ from datetime import datetime
 class ImpressionContent(Base):
     __tablename__ = 'impressioncontents'
     id = Column(Integer, primary_key=True)
+    name = Column(String(128), unique=True)
     reference1 = Column(String)
     reference2 = Column(String)
     target = Column(String)
@@ -13,7 +14,8 @@ class ImpressionContent(Base):
     ref2 = Column(Float)
     date = Column(DateTime, default=datetime.now())
 
-    def __init__(self, reference1=None, reference2=None, target=None,ref1=None, ref2=None, date=None):
+    def __init__(self, name=None, reference1=None, reference2=None, target=None,ref1=None, ref2=None, date=None):
+        self.name = name
         self.reference1 = reference1
         self.reference2 = reference2
         self.target = target
@@ -23,3 +25,17 @@ class ImpressionContent(Base):
 
     def __repr__(self):
         return '<Title %r>' % (self.title)
+
+#以下を追加
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    user_name = Column(String(128), unique=True)
+    hashed_password = Column(String(128))
+
+    def __init__(self, user_name=None, hashed_password=None):
+        self.user_name = user_name
+        self.hashed_password = hashed_password
+
+    def __repr__(self):
+        return '<Name %r>' % (self.user_name)
